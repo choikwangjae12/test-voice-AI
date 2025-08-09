f4a61902e56ed729f1ef0c984e9da55cd740cca2
LucentArtPD <88596426+choikwangjae12@users.noreply.github.com>
2025-08-09 19:27:31 +0900

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,13 @@
+# src/main.py
+import sys
+from PyQt5.QtWidgets import QApplication
+from gui import MainWindow
+
+def main():
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
